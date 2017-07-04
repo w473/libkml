ff58818542fcf23285b99a5845cd8c9a68ec3046
Jacek Głogosz <jacek.glogosz@gmail.com>
2017-07-04 20:26:39 +0200

    added extended json functionality

--- src/Entity/Feature/Container/Folder.php
+++ src/Entity/Feature/Container/Folder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace KML\Entity\Feature\Container;
 
 class Folder extends Container
@@ -6,15 +7,39 @@ class Folder extends Container
     public function __toString(): string
     {
         $parent_string = parent::__toString();
-    
+
         $output = [];
         $output[] = sprintf(
             "<Folder%s>",
-            isset($this->id)?sprintf(" id=\"%s\"", $this->id):""
+            isset($this->id) ? sprintf(" id=\"%s\"", $this->id) : ""
         );
         $output[] = $parent_string;
         $output[] = "</Folder>";
-    
+
         return implode("\n", $output);
+    }
+
+    public function jsonSerialize()
+    {
+        $jsonData = [];
+
+        if (isset($this->id)) {
+            $jsonData['id'] = $this->id;
+        }
+
+        $jsonData['type'] = 'FeatureCollection';
+        $jsonData['properties'] = [
+            'name' => $this->getName(),
+            'description' => $this->getDescription()
+        ];
+        $jsonData['features'] = [];
+
+        foreach ($this->getAllFeatures() as $feature) {
+            $jsonFeature = $feature->jsonSerialize();
+            if ($jsonFeature) {
+                $jsonData['features'][] = $jsonFeature;
+            }
+        }
+        return $jsonData;
     }
 }
--- tests/system/KMLTest.php
+++ tests/system/KMLTest.php
@@ -10,8 +10,9 @@ class KMLTest extends TestCase
 {
     const SAMPLE_TIME_KML = __DIR__ . '/../data/sample-time.kml';
     const PARSE_TEST_KML  = __DIR__ . '/../data/parse-test.kml';
+    const GOOGLE_MAP_KML  = __DIR__ . '/../data/googlemap.kml';
 
-    public function testCreateFromTextJson()
+    public function testCreateFromTextGeoJson()
     {
         $kmlData = file_get_contents(self::PARSE_TEST_KML);
 
@@ -22,6 +23,45 @@ class KMLTest extends TestCase
             '{"type":"FeatureCollection","features":[{"id":"mountainpin1","properties":' .
             '{"name":"Pin on a mountaintop","styleUrl":"#pushpin","id":"mountainpin1"},"type":' .
             '"Feature","geometry":{"type":"Point","coordinates":[170.1435558771009,-43.60505741890396]}}]}',
+            json_encode($kml->getGeoJson())
+        );
+    }
+
+    public function testCreateFromTextJson()
+    {
+        $kmlData = file_get_contents(self::PARSE_TEST_KML);
+
+        $kml = KMLBuilder::createFromText($kmlData);
+
+        $this->assertInstanceOf(KML::class, $kml);
+        $this->assertEquals(
+            '{"name":"gx:AnimatedUpdate example","description":null,"folders":[{"type":"FeatureCollection"'.
+            ',"features":[{"id":"mountainpin1","properties":{"name":"Pin on a mountaintop","styleUrl":"#pushpin",'.
+            '"id":"mountainpin1"},"type":"Feature","geometry":{"type":"Point","coordinates":[170.1435558771009,'.
+            '-43.60505741890396]}}]}]}',
+            json_encode($kml)
+        );
+    }
+
+    public function testCreateFromTextJsonLayers()
+    {
+        $kmlData = file_get_contents(self::GOOGLE_MAP_KML);
+
+        $kml = KMLBuilder::createFromText($kmlData);
+
+        $this->assertInstanceOf(KML::class, $kml);
+        $this->assertEquals(
+            '{"name":"Moja mapka","description":"opis mojej mapki","folders":[{"type":"FeatureCollection"'.
+            ',"properties":{"name":"pierwsza warstwa","description":null},"features":[{"properties":{"name":'.
+            '"punkt w pierwszej warsatwie","description":"opis\u00a0punkt w pierwszej warsatwie","styleUrl":'.
+            '"#icon-1899-0288D1"},"type":"Feature","geometry":{"type":"Point","coordinates":[10.809989,51.7279585]}}'.
+            ',{"properties":{"name":"Punkt 22","description":"pkt 12","styleUrl":"#icon-1899-0288D1"},"type":'.
+            '"Feature","geometry":{"type":"Point","coordinates":[10.8081222,51.7283971]}}]},{"type":"FeatureCol'.
+            'lection","properties":{"name":"Druga warstwa","description":null},"features":[{"properties":{"name"'.
+            ':"punkt w drugiej warsatwie","description":"punkt w drugiej warsatwie","styleUrl":"#icon-1899-0288D'.
+            '1"},"type":"Feature","geometry":{"type":"Point","coordinates":[10.8094686,51.7276063]}},{"propertie'.
+            's":{"name":"Punkt 2","description":"pkt2","styleUrl":"#icon-1899-0288D1"},"type":"Feature","geometr'.
+            'y":{"type":"Point","coordinates":[10.8089751,51.7282742]}}]}]}',
             json_encode($kml)
         );
     }
